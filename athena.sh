c28488bd0174bb2cfaef6e95bdafac863db491e7
Athena0070 <fomenkyt@gmail.com>
2023-10-02 21:29:29 -0400

    ..

---
+++ athena.sh
@@ -0,0 +1,9 @@
+#!/bib/bash
+echo -n "please enter your name: " 
+read name 
+echo "please enter your team: " 
+read team 
+echo "great news contributing o this project" 
+echo "end of contribution. Thank you!"
+
+
